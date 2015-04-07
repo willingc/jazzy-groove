29495a87e5d532a3311917cb0751141adc465776
Carol Willing <carolcode@willingconsulting.com>
2015-04-07 01:10:12 -0400

    Remove references to ipyagogo

--- conf.py
+++ conf.py
@@ -201,7 +201,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ipyagogodoc'
+htmlhelp_basename = 'jazzy-groovedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -223,7 +223,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'ipyagogo.tex', 'ipyagogo Documentation',
+  (master_doc, 'jazzy-groove.tex', 'jazzy-groove Documentation',
    'Carol Willing', 'manual'),
 ]
 
@@ -253,7 +253,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'ipyagogo', 'ipyagogo Documentation',
+    (master_doc, 'jazzy-groove', 'jazzy-groove Documentation',
      [author], 1)
 ]
 
@@ -267,8 +267,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'ipyagogo', 'ipyagogo Documentation',
-   author, 'ipyagogo', 'One line description of project.',
+  (master_doc, 'jazzy-groove', 'jazzy-groove Documentation',
+   author, 'jazzy-groove', 'One line description of project.',
    'Miscellaneous'),
 ]
 
